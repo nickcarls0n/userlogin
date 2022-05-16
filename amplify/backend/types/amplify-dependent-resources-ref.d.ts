9c590ad14005454245d95d88da2ea8291b56e335
Nick <n.carlson.design@gmail.com>
2022-05-16 14:47:50 -0700

    Setup AWS API

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "userlogin": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
